c57b5d4590320c2b052a4100ee55fac10c82a32f
Paulo Hudson <105687302+PauloHudson@users.noreply.github.com>
2024-11-13 02:20:52 -0300

    Update config.js

--- Config/config.js
+++ Config/config.js
@@ -1,25 +1,25 @@
-// Import the functions you need from the SDKs you need
-import firebase from 'firebase/app';
-import 'firebase/auth';
+// Importação da API modular do Firebase (Firebase 9 ou superior)
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';  
 
 
-// Your web app's Firebase configuration
-export const firebaseConfig = {
+
+const firebaseConfig = {
   apiKey: "AIzaSyCU4ZLqYmpCK5ZniTPMo0QrwiAVgFsUqIg",
   authDomain: "healthfit-1549c.firebaseapp.com",
   databaseURL: "https://healthfit-1549c-default-rtdb.firebaseio.com",
   projectId: "healthfit-1549c",
-  storageBucket: "healthfit-1549c.firebasestorage.app",
+  storageBucket: "healthfit-1549c.appspot.com",
   messagingSenderId: "198110380646",
   appId: "1:198110380646:web:40f770ec39db6c156da3a7",
   measurementId: "G-1KFEJJ9X5C"
 };
 
-// Inicializa o Firebase somente se ele ainda não foi inicializado
-if (!firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig);
-}else{
-  firebase.app();
-}
 
-export default firebase;
+const app = initializeApp(firebaseConfig);
+
+
+const auth = getAuth(app);
+
+
+export { auth };
