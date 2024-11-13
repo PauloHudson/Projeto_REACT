ce976515d4ea0cf849b7925ea3415c93669a744b
Paulo Hudson <105687302+PauloHudson@users.noreply.github.com>
2024-11-12 21:28:41 -0300

    Update Cadastro.js
    Firebase

--- screens/Cadastro.js
+++ screens/Cadastro.js
@@ -1,7 +1,11 @@
 import React from 'react';
-import {Text, Button } from 'react-native';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import { Text, Button, Alert } from 'react-native';
 import { View, Container, TitleList, InputField, Container2 } from '../styles/adm';
+import firebase from '../Config/config';
+
+
+
+
 
 class Cadastro extends React.Component {
   constructor(props) {
@@ -13,13 +17,32 @@ class Cadastro extends React.Component {
     };
   }
 
-  async gravar() {
-    try {
-      await AsyncStorage.setItem(this.state.user, this.state.password);
-      alert('Salvo com sucesso!!!');
-    } catch (erro) {
-      alert('Erro ao salvar!');
+  gravar() {
+    console.log("foi");
+    const { email, password } = this.state;
+    if (!email || !password) {
+      Alert.alert('Erro', 'Preencha todos os campos.');
+      return;
     }
+  
+    firebase.auth()
+      .createUserWithEmailAndPassword(email.toLowerCase(), password)
+      .then(() => {
+        Alert.alert('Sucesso', 'Usuário cadastrado com sucesso!');
+        this.props.navigation.navigate('Login');
+      })
+      .catch(error => {
+        const errorCode = error.code;
+        if (errorCode === "auth/email-already-in-use") {
+          Alert.alert('Erro', "Esse email já está em uso");
+        } else if (errorCode === "auth/weak-password") {
+          Alert.alert('Erro', "Senha fraca, escolha uma senha mais segura");
+        } else if (errorCode === "auth/invalid-email") {
+          Alert.alert('Erro', "Formato do email inválido");
+        } else {
+          Alert.alert('Erro', "Ocorreu um erro: " + error.message);
+        }
+      });
   }
   
   render() {
@@ -27,11 +50,11 @@ class Cadastro extends React.Component {
       <View>
         <Container2 source={require('../styles/Back.png')}> 
           <Text style={{ color: '#fff', fontSize: 24 }}>Bem-vindo ao Cadastro!</Text>
-          <Text style={{ color: '#fff', fontSize: 14, backgroundColor: "grey", marginTop: 20, padding: 10 }}>encontre a melhor dieta para você</Text>
+          <Text style={{ color: '#fff', fontSize: 14, backgroundColor: "grey", marginTop: 20, padding: 10 }}>Encontre a melhor dieta para você</Text>
         </Container2>
 
         <Container> 
-        <TitleList> Nome: </TitleList>
+          <TitleList> Nome: </TitleList>
           <InputField 
             placeholder="Digite seu nome"
             onChangeText={(texto) => this.setState({ user: texto })} 
