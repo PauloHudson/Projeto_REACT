6a4cb72f826f869ebacb6f384b5cf58b47d80249
Paulo Hudson <105687302+PauloHudson@users.noreply.github.com>
2024-11-12 21:28:54 -0300

    FireBase

--- screens/Principal.js
+++ screens/Principal.js
@@ -1,59 +1,67 @@
 import React from 'react';
-import { Text, TextInput, Button } from 'react-native';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import { Text, Button, Alert } from 'react-native';
 import { View, Container, TitleList, InputField, Container2 } from '../styles/adm';
+import firebase from '../Config/config';
+
+
 
 class Principal extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      usuario: '',
-      senha: '',
+      email: '',
+      password: '',
     };
   }
 
-  async ler() {
-    try {
-      let senha = await AsyncStorage.getItem(this.state.usuario);
-      if (senha != null) {
-        if (senha === this.state.senha) {
-          console.log('Usuário:', this.state.usuario); 
-          this.props.navigation.navigate('Tela3', { nome: this.state.usuario }); // Passando o nome
-        } else {
-          alert('Senha Incorreta!');
-        }
-      } else {
-        alert('Usuário não foi encontrado!');
-      }
-    } catch (erro) {
-      console.log(erro);
+  ler() {
+    console.log("logado");
+    const { email, password } = this.state;
+    
+    if (!email || !password) {
+      console.log("logado");
+      Alert.alert('Erro', 'Preencha todos os campos.');
+      return;
     }
+
+    firebase.auth()
+      .signInWithEmailAndPassword(this.state.email.toLowerCase(), this.state.password)
+      .then(() => {
+        Alert.alert('Sucesso', 'Login realizado com sucesso!');
+        this.props.navigation.navigate('Tela3', { nome: this.state.email });
+      })
+      .catch(error => {
+        const errorCode = error.code;
+        if (errorCode === "auth/invalid-email") {
+          Alert.alert('Erro', "Formato do email inválido");
+        } else if (errorCode === "auth/user-not-found") {
+          Alert.alert('Erro', "Usuário não encontrado");
+        } else {
+          Alert.alert('Erro', "Ocorreu um erro: " + error.message);
+        }
+      });
   }
 
   render() {
     return (
       <View>
         <Container2 source={require('../styles/Back.png')}> 
-          <Text style={{ color: '#fff', fontSize: 24 }}>Bem vindo ao Health FIT!</Text>
-          <Text style={{ color: '#fff', fontSize: 14, backgroundColor: "grey", marginTop: 20, padding: 10 }}>encontre a melhor dieta para você</Text>
+          <Text style={{ color: '#fff', fontSize: 24 }}>Bem-vindo ao Health FIT!</Text>
+          <Text style={{ color: '#fff', fontSize: 14, backgroundColor: "grey", marginTop: 20, padding: 10 }}>Encontre a melhor dieta para você</Text>
         </Container2>
 
         <Container> 
-          <TitleList> Usuário: </TitleList>
+          <TitleList> E-mail: </TitleList>
           <InputField 
-            placeholder="Digite seu nome"
-            onChangeText={(texto) => this.setState({ usuario: texto })} 
+            placeholder="Digite seu e-mail"
+            onChangeText={(texto) => this.setState({ email: texto })} 
           />
-          <TitleList> Senha: </TitleList>
-
-         
+          <TitleList> password: </TitleList>
           <InputField 
             secureTextEntry 
-            placeholder="Digite sua senha"
-            onChangeText={(texto) => this.setState({ senha: texto })} 
+            placeholder="Digite sua password"
+            onChangeText={(texto) => this.setState({ password: texto })} 
           />
-
-
           <Button title="Logar" onPress={() => this.ler()} />
         </Container>
       </View>
